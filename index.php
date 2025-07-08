3d0071babacef8a0b625bd726e8159456901ee5f
Daniels <134560764+Daniel-Sequeira@users.noreply.github.com>
2025-07-08 16:54:26 -0600

    Readme actualizar

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require_once 'system/view.php';
 require_once 'system/app.php';
 require_once 'config/config.php';
 
+//Instancia de la clase App, que es el enrutador del sistema.
 $app = new App();
 
 ?>
